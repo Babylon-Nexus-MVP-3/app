da5f517ee3a958c9374dc87568741c8f6f92fa30
Mubashir Hussain <mubashirmh04@gmail.com>
2026-02-20 09:52:42 +1100

    chore: Configure Swagger UI in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,10 @@
 import express, { json, Request, Response } from "express";
 import cors from "cors";
 import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yaml";
+import path from "path";
+import fs from "fs";
 
 export const app = express();
 
@@ -8,3 +12,11 @@ app.use(json());
 app.use(morgan("dev"))
 app.use(cors());
 
+// Render API Documentation
+const swaggerFile = fs.readFileSync(path.join(__dirname, '../swagger.yaml'), 'utf8');
+const swaggerDoc = YAML.parse(swaggerFile);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.get('/', (req, res) => {
+  res.redirect('/api-docs');
+});
