ad0a0b9dba3650732f175fe016f440b8a694e6e0
Mubashir Hussain <mubashirmh04@gmail.com>
2026-02-27 15:57:20 +1100

    feat: implement rate limiting

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import * as AuthController from "../controllers/auth.controller";
+import { registrationLimiter } from "../middleware";
 
 export const authRouter = express.Router();
 
-authRouter.post("/register", AuthController.register);
+authRouter.post("/register", registrationLimiter, AuthController.register);
