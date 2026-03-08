4e07874b37d76f163d42af0848ed3738149d505d
Mubashir Hussain <mubashirmh04@gmail.com>
2026-03-08 12:09:38 +1100

    feat(models): add projectParticipant model to map subbies to projects

---
+++ backend/src/models/projectParticipantModel.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface ProjectParticipant extends Document {
+  projectId: string;
+  userId: string;
+  role: string;
+  email: string;
+  inviteCode: string;
+  trade: string;
+  dateInvited: Date;
+  status: "Pending" | "Accepted";
+}
+
+export const ProjectParticipantSchema = new Schema<ProjectParticipant>({
+  // @ts-expect-error Mongoose ObjectId typing mismatch between runtime and @types
+  projectId: { type: Schema.Types.ObjectId, ref: "Project", required: true },
+  // @ts-expect-error Mongoose ObjectId typing mismatch between runtime and @types
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  role: { type: String },
+  email: { type: String, required: true },
+  inviteCode: { type: String },
+  trade: { type: String, required: true },
+  dateInvited: { type: Date },
+  status: { type: String },
+});
+
+export const ProjectParticipantModel = mongoose.model<ProjectParticipant>(
+  "ProjectParticipant",
+  ProjectParticipantSchema
+);
