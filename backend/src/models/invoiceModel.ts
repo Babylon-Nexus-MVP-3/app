183a0f5009c1ad3c4bfa2560ef651648b967cc52
Mubashir Hussain <mubashirmh04@gmail.com>
2026-03-08 11:25:10 +1100

    feat(models): add invoiceModel fields

--- backend/src/models/invoiceModel.ts
+++ backend/src/models/invoiceModel.ts
@@ -1,17 +1,29 @@
 import mongoose, { Schema, Document } from "mongoose";
 
+export type InvoiceStatus = "Pending" | "Approved" | "Paid" | "Recieved";
+
 export interface Invoice extends Document {
+  projectId: string;
   submittingParty: string;
   submittingCategory: string;
-  dateDue: Date;
   description: string;
+  dateSubmitted: Date;
+  dateDue: Date;
+  datePaid: Date;
+  dateRecieved: Date;
+  status: InvoiceStatus;
 }
 
 const invoiceSchema = new Schema<Invoice>({
+  projectId: { type: String, required: true },
   submittingParty: { type: String, required: true },
   submittingCategory: { type: String, required: true },
-  dateDue: { type: Schema.Types.Date, required: true },
   description: { type: String, required: true },
+  dateSubmitted: { type: Schema.Types.Date, required: true },
+  dateDue: { type: Schema.Types.Date },
+  datePaid: { type: Schema.Types.Date },
+  dateRecieved: { type: Schema.Types.Date },
+  status: { type: String, enum: ["Pending", "Approved", "Paid", "Recieved"] },
 });
 
 export const InvoiceModel = mongoose.model<Invoice>("Invoice", invoiceSchema);
