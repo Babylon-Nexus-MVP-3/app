51106c155f3fa6643d0dd994423053dce1970e9d
Mubashir Hussain <mubashirmh04@gmail.com>
2026-03-12 18:29:51 +1100

    fix: update invite endpoint to allow all users to create an invite

--- backend/src/routes/project.route.ts
+++ backend/src/routes/project.route.ts
@@ -1,14 +1,8 @@
 import express from "express";
 import * as ProjectController from "../controllers/project.controller";
-import { requireAuth, requireRole } from "../middleware";
+import { requireAuth } from "../middleware";
 
 export const projectRouter = express.Router();
 
 projectRouter.post("/", requireAuth, ProjectController.create);
-projectRouter.post("/:projectId/invite", requireAuth, requireRole("PM"), ProjectController.invite);
-projectRouter.post(
-  "/:projectId/accept",
-  requireAuth,
-  requireRole("Subbie", "Owner", "Builder", "Consultant"),
-  ProjectController.acceptInvite
-);
+projectRouter.post("/:projectId/invite", requireAuth, ProjectController.invite);
