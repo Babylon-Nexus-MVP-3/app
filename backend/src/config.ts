6bb37d6bfdc569765e766214ea43a099278c808f
Mubashir Hussain <mubashirmh04@gmail.com>
2026-04-04 14:15:18 +1100

    fix: add non null assertion

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
-  jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
-  jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
+  jwtAccessSecret: process.env.JWT_ACCESS_SECRET!,
+  jwtRefreshSecret: process.env.JWT_REFRESH_SECRET!,
   accessTokenTtlMinutes: Number(process.env.ACCESS_TOKEN_TTL_MINUTES),
   refreshTokenTtlDays: Number(process.env.REFRESH_TOKEN_TTL_DAYS),
 };
