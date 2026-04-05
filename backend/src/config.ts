755e0917a3598967988c78bfdf79a5b4ae87af11
Mubashir Hussain <mubashirmh04@gmail.com>
2026-04-05 12:09:27 +1000

    chore: update config

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,9 +1,6 @@
-import dotenv from "dotenv";
-dotenv.config(); // Load it here!
-
 export const config = {
   jwtAccessSecret: process.env.JWT_ACCESS_SECRET || "fallback-secret-for-tests",
   jwtRefreshSecret: process.env.JWT_REFRESH_SECRET || "fallback-refresh-for-tests",
-  accessTokenTtlMinutes: Number(process.env.ACCESS_TOKEN_TTL_MINUTES),
-  refreshTokenTtlDays: Number(process.env.REFRESH_TOKEN_TTL_DAYS),
+  accessTokenTtlMinutes: Number(process.env.ACCESS_TOKEN_TTL_MINUTES) || 10,
+  refreshTokenTtlDays: Number(process.env.REFRESH_TOKEN_TTL_DAYS) || 1,
 };
